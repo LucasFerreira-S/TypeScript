86cc7db86eb037f8e0983134e6008542d6fa9789
lucas <lucas.santos121252@gmail.com>
2022-04-18 08:24:37 -0300

    update: Aula 3

---
+++ app/models/negociacao.ts
@@ -0,0 +1,26 @@
+export class Negociacao{
+
+    private _data;
+    private _quatidade;
+    private _valor;
+
+    constructor(data,quantidade,valor){
+        this._data = data;
+        this._quatidade = quantidade;
+        this._valor = valor;
+
+    }
+
+    get data(){
+        return this._data;
+    }
+    get quantidade(){
+        return this._quatidade;
+    }
+    get valor(){
+        return this._valor;
+    }
+    get volume(){
+        return this._quatidade * this._valor;
+    }
+}
---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,19 @@
+export class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quatidade = quantidade;
+        this._valor = valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quatidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quatidade * this._valor;
+    }
+}
